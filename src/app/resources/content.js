ab19564765aa14c5b336ae4428f8db011c23908c
kbakes40 <kbakes40@github.com>
2026-02-24 03:14:56 -0500

    Remove Once UI attribution, hide blog, update LinkedIn URL

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -14,7 +14,7 @@ const social = [
   {
     name: "LinkedIn",
     icon: "linkedin",
-    link: "https://www.linkedin.com/in/kevin-baker",
+    link: "https://www.linkedin.com/in/kevin-j-baker",
   },
   {
     name: "Email",
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Flex, IconButton, SmartLink, Text } from "@/once-ui/components";
+import { Flex, IconButton, Text } from "@/once-ui/components";
 import { person, social } from "@/app/resources/content";
 import styles from "./Footer.module.scss";
 
@@ -23,17 +23,8 @@ export const Footer = () => {
         vertical="center"
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
-          <Text onBackground="neutral-weak">© {currentYear} /</Text>
+          <Text onBackground="neutral-weak">© {currentYear}</Text>
           <Text paddingX="4">{person.name}</Text>
-          <Text onBackground="neutral-weak">
-            {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Build your portfolio with{" "}
-            <SmartLink
-              href="https://once-ui.com/templates/magic-portfolio"
-            >
-              Once UI
-            </SmartLink>
-          </Text>
         </Text>
         <Flex gap="16">
           {social.map(
